0f06262cb3606212d3f7d22a050fa8b5a86eaaf9
Conor Finn <cf338@kent.ac.uk>
2020-12-08 18:48:17 +0000

    Refactor movement code and add comments RE #36

--- project/sherLOCKED/Assets/Scripts/Sherry/SherryMovement.cs
+++ project/sherLOCKED/Assets/Scripts/Sherry/SherryMovement.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class SherryMovement : MonoBehaviour
 {
-
     // Publically Acessible variables
     public float walkSpeed = 3f;
 
-    // Sprites for movement directions
-    public Sprite northSpr;
-    public Sprite eastSpr;
-    public Sprite southSpr;
-    public Sprite westSpr;
-
-	Direction facing;
+    // Movement control variables
 	Vector2 input;
 	bool isMoving = false;
     Vector3 startPos;
     Vector3 endPos;
     float time;
 
+    // Animation controller
     Animator anim;
 
+    // Called at start
     void Start() {
         anim = GetComponent<Animator>();
     }
@@ -52,12 +47,14 @@ public class SherryMovement : MonoBehaviour
         }
     }
 
+    /**
+    * Move the character smoothly according to the input vector
+    * @ entity The entity to be moved
+    */
     public IEnumerator Move(Transform entity) {
         isMoving = true;
         startPos = entity.position;
         time = 0;
-
-        print(startPos);
 
         endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
 
@@ -70,11 +67,4 @@ public class SherryMovement : MonoBehaviour
         isMoving = false;
         yield return 0;
     }
-}
-
-enum Direction {
-	North, 
-	East, 
-	South, 
-	West
 }
