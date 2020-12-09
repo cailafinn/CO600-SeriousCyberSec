6eb2574c6a3be1a37e3e978303fa63e08370d356
Alice Jaffray <aj418@kent.ac.uk>
2020-12-09 22:49:20 +0000

    Sherry interacting with desk RE #37

---
+++ project/sherLOCKED/Assets/Scripts/Sherry/SherryInteract.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SherryInteract : MonoBehaviour
+{
+    public Text interact;
+    private bool inCollider = false;
+    private Rigidbody2D rb2d;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (inCollider)
+        {
+            if (Input.GetKeyDown("e") || Input.GetKeyDown("space"))
+            {
+                interact.text = "Good Job. You did it!";
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Interactable") && !inCollider)
+        {
+            inCollider = !inCollider;
+            interact.text = "Press E or Space to interact with the desk.";
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            inCollider = !inCollider;
+            interact.text = "";
+        }
+    }
+
+}
