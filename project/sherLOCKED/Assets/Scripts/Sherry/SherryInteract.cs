ffb1cc2eb0d0e9dec0217f3758a1322941a86ae7
Alice Jaffray <aj418@kent.ac.uk>
2020-12-10 01:14:19 +0000

    Removed redundant code RE #37

--- project/sherLOCKED/Assets/Scripts/Sherry/SherryInteract.cs
+++ project/sherLOCKED/Assets/Scripts/Sherry/SherryInteract.cs
@@ -8,13 +8,6 @@ public class SherryInteract : MonoBehaviour
 {
     public Text interact;
     private bool inCollider = false;
-    private Rigidbody2D rb2d;
-
-
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
 
     void Update()
     {
