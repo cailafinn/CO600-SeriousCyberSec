c4a8e2809a76a0b79e012371652bde42cac6cd8c
Conor Finn <cf338@kent.ac.uk>
2020-12-14 13:10:42 +0000

    Add question UI when interacting with PC RE #38
    Added a question prompt. Some feedback is also given to the user when
    they make a selection. For now, the state of the selection is maintained
    within the run. In order to do the question again, the game must be
    restarted.

---
+++ project/sherLOCKED/Assets/Scripts/Objects/ComputerInteraction.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerInteraction : MonoBehaviour
+{
+    // Public Variables
+    public Image yesButton;
+    public Image noButton;
+    public Text extraInfo;
+    
+    // Private Variables
+    private GameObject[] questionObjects;
+    private bool inCollider = true;
+    private bool answered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionObjects = GameObject.FindGameObjectsWithTag("ShowAfterInteraction");
+        HideQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inCollider)
+        {
+            if (Input.GetKeyDown("e") || Input.GetKeyDown("space"))
+            {
+                Time.timeScale = 0;
+                ShowQuestion();
+            }
+        }
+    }
+
+    public void Correct() {
+        if(!answered) {
+            yesButton.color = Color.green;
+            extraInfo.text = "Correct! This question is working.";
+            answered = true;
+        }
+    }
+
+    public void Incorrect() {
+        if (!answered) {
+            noButton.color = Color.red;
+            extraInfo.text = "Incorrect. This question is working.";
+            answered = true;
+        }
+    }
+
+    // Resume the scene 
+	public void Resume() {
+         if (Time.timeScale == 0){
+				Time.timeScale = 1;
+                HideQuestion();
+			}
+	}
+
+    // Shows objects with the ShowAfterInteraction tag
+    private void ShowQuestion() {
+        foreach (GameObject g in questionObjects) {
+            g.SetActive(true);
+        }
+    }
+
+    // Hides objects with the ShowAfterInteraction tag
+    private void HideQuestion() {
+        foreach (GameObject g in questionObjects) {
+            g.SetActive(false);
+        }
+    }
+    
+    // checks if player is near object
+    // if they are near the object, text appears
+    // prompting user to interact with object
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !inCollider)
+        {
+            inCollider = true;
+        }
+    }
+
+    // checks if player leaves the object perimeter
+    // if player does leave the perimeter text prompt is removed
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inCollider = false;
+        }
+    }
+}
