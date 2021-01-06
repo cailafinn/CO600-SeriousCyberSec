3974611de67a01a176a122df737dafe4050518dd
Conor Finn <cf338@kent.ac.uk>
2021-01-06 14:42:00 +0000

    Create basic main menu RE #45

---
+++ project/sherLOCKED/Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject howToMenu;
+
+    public void Start() {
+        howToMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    // Start the game
+    public void PlayGame() {
+        SceneManager.LoadScene("Hall");
+    }
+
+    // Show the controls and hide the main menu
+    public void HowToPlay() {
+        mainMenu.SetActive(false);
+        howToMenu.SetActive(true);
+    }
+
+    // Return to the main menu from the controls. 
+    public void Back() {
+        howToMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
