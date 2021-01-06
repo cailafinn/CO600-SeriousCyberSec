3c157ab937b1d59fe7c02e12a67f5abd93b5ceea
Conor Finn <cf338@kent.ac.uk>
2021-01-06 15:26:52 +0000

    Create level select menu RE #45

--- project/sherLOCKED/Assets/Scripts/Menus/MainMenuController.cs
+++ project/sherLOCKED/Assets/Scripts/Menus/MainMenuController.cs
@@ -7,15 +7,18 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject howToMenu;
+    public GameObject levelSelectMenu;
 
     public void Start() {
         howToMenu.SetActive(false);
+        levelSelectMenu.SetActive(false);
         mainMenu.SetActive(true);
     }
 
     // Start the game
     public void PlayGame() {
-        SceneManager.LoadScene("Hall");
+        mainMenu.SetActive(false);
+        levelSelectMenu.SetActive(true);
     }
 
     // Show the controls and hide the main menu
@@ -24,9 +27,30 @@ public class MainMenuController : MonoBehaviour
         howToMenu.SetActive(true);
     }
 
-    // Return to the main menu from the controls. 
+    // Return to the main menu. 
     public void Back() {
         howToMenu.SetActive(false);
+        levelSelectMenu.SetActive(false);
         mainMenu.SetActive(true);
+    }
+
+    public void level1() {
+        SceneManager.LoadScene("Hall");
+    }
+
+    public void level2() {
+        
+    }
+
+    public void level3() {
+
+    }
+
+    public void level4() {
+
+    }
+
+    public void level5() {
+
     }
 }
