fd7e1312f6fed971ebaa744c41bc27c39dc976a7
Conor Finn <cf338@kent.ac.uk>
2021-01-16 18:31:11 +0000

    Add script and discussion to prefab RE #77

--- project/sherLOCKED/Assets/Scripts/UI/TextBoxController.cs
+++ project/sherLOCKED/Assets/Scripts/UI/TextBoxController.cs
@@ -12,31 +12,47 @@ public class TextBoxController : MonoBehaviour
     // Flag to say if text represents thoughts rather than speech
     private bool thought = false;
 
-    // Is the left sprite in focus.
-    private bool leftFocus;
-
-    // Is the right sprite in focus.
-    private bool rightFocus;
-
     // Start is called before the first frame update
 
     private Color light = new Color(1, 1, 1);
     private Color dark = new Color(0.25f, 0.25f, 0.25f);
 
+    private string[] story = new string[]{"You must be Ginny, you called my office about a hacking?", "Yes. That is correct. Someone took control of the house!", "We had all these improvements installed and they just went haywire last night.", "Interesting, I'll have a look around and see what I can find.", "Once I've gained enough INTUITION, I should be able to work out what really happened here.", "You are supposed to be the best in the business, I hope you live up to your REPUTATION.", "Of course Ma'am, I'll have this case cracked in no time!", "(I should make sure my deductions are correct. Too many errors and I'll be fired!)", "Best of luck to you, Sherry.", ""};
+    private List<bool[]> focuses = new List<bool[]> {new bool[] {true,false}, new bool[] {false, true}, new bool[] {false, true}, new bool[] {true,false}, new bool[] {true,false}, new bool[] {false,true}, new bool[] {true,false}, new bool[] {true,false}, new bool[] {false,true}, new bool[] {false, false}};
+    
+    private int currentLine = 0;
     void Start()
     {
-        leftFocus = false;
-        rightFocus = false;
-        Focus();
+        Focus(true, false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        text.text = "AAAAAA";
+    void Awake() {
+        StartCoroutine("Write");
     }
 
-    void Focus() {
+    public void NextText() {
+        StopAllCoroutines();
+        currentLine++;
+        Focus(focuses[currentLine][0], focuses[currentLine][1]);
+        StartCoroutine("Write");
+        if (currentLine == story.Length-1) {
+            Close();
+        }
+    }
+
+    IEnumerator Write() {
+        text.text = "";
+        foreach (char c in story[currentLine]) {
+            text.text += c;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    void Close() {
+        GameObject.Find("TextBox").SetActive(false);
+    }
+
+    void Focus(bool leftFocus, bool rightFocus) {
         if (leftFocus) {
             leftSprite.color = new Color(1, 1, 1);
         } else {
