241c2ca8252c9a5185c7ea53d4c40fcf2e2c2102
Conor Finn <cf338@kent.ac.uk>
2021-01-25 13:16:38 +0000

    Add script for question manager RE #91

---
+++ project/sherLOCKED/Assets/Scripts/_preload/QuestionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    // Singleton instance accessor
+    public static QuestionManager Instance { get; private set;}
+    
+    private List<GameObject> interacted = new List<GameObject>();
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+
+    public bool IsAnswered(GameObject toCheck) {
+        return interacted.Contains(toCheck);
+    }
+
+    public void ClearList() {
+        interacted.Clear();
+    }
+
+    public void AddAnswered(GameObject toAdd) {
+        interacted.Add(toAdd);
+    }
+}
