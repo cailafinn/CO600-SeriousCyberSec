931be78b1ab1a3812f312e54b10323b1a5133699
Conor Finn <cf338@kent.ac.uk>
2021-01-26 16:50:37 +0000

    Add Level end code to ScoreManager RE #96

--- project/sherLOCKED/Assets/Scripts/_preload/ScoreManager.cs
+++ project/sherLOCKED/Assets/Scripts/_preload/ScoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class ScoreManager : MonoBehaviour
     private int maxRep = 50;
     private int nextInt = 10;
 
+    private DateTime levelStart;
+
+    private TimeSpan levelTime;
+
     void Awake() {
         if (Instance == null) {
             Instance = this;
@@ -24,9 +29,11 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         if (currentRep <= 0) {
-            print("LOSE!");
+            LevelEnd();
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
         } else if (currentInt >= maxInt){
-            print("WIN!");
+            LevelEnd();
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelEnd");
         }
     }
 
@@ -43,9 +50,15 @@ public class ScoreManager : MonoBehaviour
     }
 
     public void Reset() {
+        levelStart = DateTime.Now;
+        levelTime = TimeSpan.Zero;
         currentInt = 0;
         currentRep = maxRep;
         GameObject.Find("ReputationBar").GetComponent<ProgressBar>().SetValue(currentRep);
         GameObject.Find("IntuitionBar").GetComponent<ProgressBar>().SetValue(currentInt);
+    }
+
+    private void LevelEnd() {
+        levelTime = DateTime.Now - levelStart;
     }
 }
