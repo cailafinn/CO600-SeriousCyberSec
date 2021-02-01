bfa7a67e9adfca68cd029d552eef3c74b3835b44
Conor Finn <cf338@kent.ac.uk>
2021-02-01 13:05:35 +0000

    Add initial LevelManager script RE #108

---
+++ project/sherLOCKED/Assets/Scripts/_preload/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set;}
+    private Dictionary<int, bool> levels = new Dictionary<int, bool>();
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+
+    void Start() {
+        levels.Add(1, true);
+        levels.Add(2, false);
+        levels.Add(3, false);
+        levels.Add(4, false);
+        levels.Add(5, false);
+    }
+
+    public bool GetComplete(int level) {
+        return levels[level];
+    }
+
+    public void SetComplete(int level) {
+        levels[level] = true;
+    }
+}
