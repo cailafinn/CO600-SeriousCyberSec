6e9b2fb797a1b89c8b5d656a03f54a238956dd40
Conor Finn <cf338@kent.ac.uk>
2021-02-08 12:36:58 +0000

    Create music manager script RE #121

---
+++ project/sherLOCKED/Assets/Scripts/_preload/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource MusicSource;
+
+    public static MusicManager Instance { get; private set;}
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+
+    public void PlayMusic(AudioClip clip) {
+        MusicSource.clip = clip;
+        MusicSource.Play();
+    }
+}
