a837a042b273efa0413d140ebd809153da022fe0
Conor Finn <cf338@kent.ac.uk>
2021-02-08 12:11:06 +0000

    Create introduction convo and refactor RE #126
    Allows conversations to be added more easily by making the list of words
    and the focus able to be input from the Unity edtior.

--- project/sherLOCKED/Assets/Scripts/HallStart.cs
+++ project/sherLOCKED/Assets/Scripts/HallStart.cs
@@ -7,15 +7,16 @@ public class HallStart : MonoBehaviour
     // Reference to the Sherry prefab
     public GameObject sherryPrefab;
 
+    public TextBoxController introText;
+
     void Start() {
         if(GameObject.FindGameObjectsWithTag("Player").Length == 0) {
             // Instantiate player when the game is loaded
             var sherry = Instantiate(sherryPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            sherry.GetComponent<SherryProgress>().intuitionBar = GameObject.Find("IntuitionBar").GetComponent<ProgressBar>();  
-            sherry.GetComponent<SherryProgress>().reputationBar = GameObject.Find("ReputationBar").GetComponent<ProgressBar>();
             sherry.transform.tag = "Player";
             sherry.GetComponent<Renderer>().sortingLayerName = "character";
             sherry.GetComponent<Renderer>().sortingOrder = 0;
         }
+        introText.Interact();
     }
 }
--- project/sherLOCKED/Assets/Scripts/UI/TextBoxController.cs
+++ project/sherLOCKED/Assets/Scripts/UI/TextBoxController.cs
@@ -9,8 +9,8 @@ public class TextBoxController : MonoBehaviour
     public Image rightSprite;
     public TextMeshProUGUI text;
 
-    private string[] story = new string[]{"You must be Ginny, you called my office about a hacking?", "Yes. That is correct. Someone took control of the house!", "We had all these improvements installed and they just went haywire last night.", "Interesting, I'll have a look around and see what I can find.", "Once I've gained enough INTUITION, I should be able to work out what really happened here.", "You are supposed to be the best in the business, I hope you live up to your REPUTATION.", "Of course Ma'am, I'll have this case cracked in no time!", "(I should make sure my deductions are correct. Too many errors and I'll be fired!)", "Best of luck to you, Sherry.", ""};
-    private List<bool[]> focuses = new List<bool[]> {new bool[] {true,false}, new bool[] {false, true}, new bool[] {false, true}, new bool[] {true,false}, new bool[] {true,false}, new bool[] {false,true}, new bool[] {true,false}, new bool[] {true,false}, new bool[] {false,true}, new bool[] {false, false}};
+    public string[] story = new string[0];
+    public bool[] focusLeft = new bool[0];
     
     private int currentLine = 0;
 
@@ -21,7 +21,7 @@ public class TextBoxController : MonoBehaviour
         playerMovement = GameObject.Find("sherry_b1(Clone)").GetComponent<SherryMovement>();
         if (!open) {
             playerMovement.enabled = false;
-            Focus(focuses[currentLine][0], focuses[currentLine][1]);
+            Focus(focusLeft[currentLine]);
             StartCoroutine("Write");
         }
     }
@@ -29,7 +29,7 @@ public class TextBoxController : MonoBehaviour
     public void NextText() {
         StopAllCoroutines();
         currentLine++;
-        Focus(focuses[currentLine][0], focuses[currentLine][1]);
+        Focus(focusLeft[currentLine]);
         StartCoroutine("Write");
         if (currentLine == story.Length-1) {
             GameObject.Find("TextBox").SetActive(false);
@@ -46,16 +46,14 @@ public class TextBoxController : MonoBehaviour
             yield return new WaitForSeconds(0.02f);
         }
     }
-    void Focus(bool leftFocus, bool rightFocus) {
+
+    void Focus(bool leftFocus) {
         if (leftFocus) {
             leftSprite.color = new Color(1, 1, 1);
+            rightSprite.color = new Color(0.25f, 0.25f, 0.25f);
         } else {
             leftSprite.color = new Color(0.25f, 0.25f, 0.25f);
-        }
-        if (rightFocus) {
             rightSprite.color = new Color(1, 1, 1);
-        } else {
-            rightSprite.color = new Color(0.25f, 0.25f, 0.25f);
         }
     }
 }
