f0b0eb9e0cdc5d1950d442831a7b79d94e4d3da8
Conor Finn <cf338@kent.ac.uk>
2021-02-09 14:56:55 +0000

    Create colour manager script RE #94

---
+++ project/sherLOCKED/Assets/Scripts/_preload/ColourManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourManager : MonoBehaviour
+{
+    public static ColourManager Instance { get; private set;}
+
+    public RuntimeAnimatorController[] controllers;
+
+    private RuntimeAnimatorController currentController;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+
+    void Start() {
+        currentController = controllers[0];
+    }
+
+    public void SetColour(string colour) {
+        if (colour == "grey") {
+            currentController = controllers[0];
+        } else if (colour == "red") {
+            currentController = controllers[1];
+        } else if (colour == "green") {
+            currentController = controllers[2];
+        } else if (colour == "blue") {
+            currentController = controllers[3];
+        } else {
+            Debug.Log("Error: Invalid colour controller selected. 'grey', 'red', 'green', or 'blue' are the only valid options.");
+        }
+    }
+    
+    public RuntimeAnimatorController GetCurrentController() {
+        return currentController;
+    }
+}
