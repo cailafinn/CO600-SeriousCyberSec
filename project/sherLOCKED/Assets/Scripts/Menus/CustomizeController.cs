ae8d43c2eb34f87a1bc2fee00ef4a098fc6f4010
Conor Finn <cf338@kent.ac.uk>
2021-02-09 15:25:06 +0000

    Implement customization into main menu RE #94

---
+++ project/sherLOCKED/Assets/Scripts/Menus/CustomizeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeController : MonoBehaviour
+{
+    public void SetGrey() {
+        ColourManager.Instance.SetColour("grey");
+    }
+
+    public void SetRed() {
+        ColourManager.Instance.SetColour("red");
+    }
+
+    public void SetGreen() {
+        ColourManager.Instance.SetColour("green");
+    }
+
+    public void SetBlue() {
+        ColourManager.Instance.SetColour("blue");
+    }
+}
